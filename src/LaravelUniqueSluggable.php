accc9308d518102742775106d74a3f07f82ed228
Oğuzcan Demircan <oguzcandemircan@gmail.com>
2021-07-23 23:53:03 +0300

    fix route priority
    Now the routes should be registered with the ```registerRoutes``` method.

--- src/LaravelUniqueSluggable.php
+++ src/LaravelUniqueSluggable.php
@@ -4,5 +4,9 @@ namespace OguzcanDemircan\LaravelUniqueSluggable;
 
 class LaravelUniqueSluggable
 {
-    
+
+    public function registerRoute()
+    {
+        require __DIR__ . DIRECTORY_SEPARATOR . 'routes.php';
+    }
 }
