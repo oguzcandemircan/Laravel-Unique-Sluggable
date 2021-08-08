0ec332064c0ff0d71b404fee01e5cffde1af47f9
Oğuzcan Demircan <oguzcandemircan@gmail.com>
2021-08-08 05:42:41 +0300

    add register_route_automatically config

--- config/laravel-unique-sluggable.php
+++ config/laravel-unique-sluggable.php
@@ -1,5 +1,9 @@
 <?php
 
 return [
-    //
+    /**
+     * if value is true, package register routes automatically
+     * or you must register route manually with LaravelUniqueSluggable::register() method
+     */
+    'register_route_automatically' => true,
 ];
--- src/routes.php
+++ src/routes.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use OguzcanDemircan\LaravelUniqueSluggable\Controllers\SlugController;
 
-Route::fallback(function (string $slug) {
-    $params = request()->segments();
-    app(SlugController::class)->index(request(), end($params), $params);
-});
+if (config('laravel-unique-sluggable.register_route_automatically')) {
+    Route::fallback(function (string $slug) {
+        $params = request()->segments();
+        app(SlugController::class)->index(request(), end($params), $params);
+    });
+}
