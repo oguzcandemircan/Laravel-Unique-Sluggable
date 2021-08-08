14fdc104e4cade20c9f2d4e334c1e41d68d7cd36
Oğuzcan Demircan <oguzcandemircan@gmail.com>
2021-08-08 06:52:13 +0300

    typo

--- src/routes.php
+++ src/routes.php
@@ -6,6 +6,6 @@ use OguzcanDemircan\LaravelUniqueSluggable\Controllers\SlugController;
 if (config('laravel-unique-sluggable.register_route_automatically')) {
     Route::fallback(function (string $slug) {
         $params = request()->segments();
-        app(SlugController::class)->index(request(), end($params), $params);
+        return app(SlugController::class)->index(request(), end($params), $params);
     });
 }
