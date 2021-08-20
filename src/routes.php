bd541ea5b7c23ebd7f0607665cdf59849933f4c4
Oğuzcan Demircan <oguzcandemircan@gmail.com>
2021-08-20 08:16:23 +0300

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 use OguzcanDemircan\LaravelUniqueSluggable\Controllers\SlugController;
 
 if (config('laravel-unique-sluggable.register_route_automatically')) {
-    Route::middleware('web')->fallback(function (string $slug) {
+    Route::fallback(function (string $slug) {
         $params = request()->segments();
         return app(SlugController::class)->index(request(), end($params), $params);
-    });
+    })->middleware('web');
 }
