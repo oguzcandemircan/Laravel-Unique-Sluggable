ce343f9ab73312c299d5c25fa941a0dba5e53bf0
Oğuzcan Demircan <oguzcandemircan@gmail.com>
2021-08-20 07:46:28 +0300

    add middleware web

--- src/routes.php
+++ src/routes.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use OguzcanDemircan\LaravelUniqueSluggable\Controllers\SlugController;
 
 if (config('laravel-unique-sluggable.register_route_automatically')) {
-    Route::fallback(function (string $slug) {
+    Route::middleware('web')->fallback(function (string $slug) {
         $params = request()->segments();
         return app(SlugController::class)->index(request(), end($params), $params);
     });
