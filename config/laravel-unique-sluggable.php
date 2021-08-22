f005a29b4fdfbe773183cc710bc9191cdb7701bf
Oğuzcan Demircan <oguzcandemircan@gmail.com>
2021-08-22 07:15:02 +0300

    add config

--- config/laravel-unique-sluggable.php
+++ config/laravel-unique-sluggable.php
@@ -6,4 +6,14 @@ return [
      * or you must register route manually with LaravelUniqueSluggable::register() method
      */
     'register_route_automatically' => true,
+
+    'default_slug_source' => 'title',
+
+    'seperator' => '-',
+
+    'langauge' => app()->getLocale(),
+
+    'if_slug_exists_get_validation_error' => false,
+
+    'update_slug_when_model_is_updated' => false,
 ];
